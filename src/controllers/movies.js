363f20e66c4eab52971e207037dcbe394d96c81a
ShbairAli <ali89freelancer@gmail.com>
2022-08-17 14:16:41 +0300

    fetched data from api successfully, and send it back to front to be rendered relates #9

--- src/controllers/movies.js
+++ src/controllers/movies.js
@@ -1,44 +1,50 @@
 const path = require('path');
-// const fetch = require('node-fetch')
+const fetch = require('node-fetch')
 
 
 const getMovies = (req, res) => {
     console.log("Get is Working");
+
     res.sendFile(path.join(__dirname, '..', '..', 'public', 'movies.html'));
 };
+
+const getApiMovies = (req, res) => {
+    // fetch movies from other endpoint
+    // send it to front
+}
+
 
 
 const postMovies = (req, res) => {
     console.log('Post is Working:', req.body.name);
+    const query = req.body.name
+    fetchMovies(query)
+        .then(data => data.json())
+        .then(data => res.send(data))
+
+        // .then(data => console.log(data))
+        // .then(data => res.redirect('/movies'))
+        // .then(data => console.log('fetched data:', data))
+        .catch(err => console.log(err))
 
 
-
-    // fetchMovies(req.body)
-    //     .then(res => res.json())
-    //     .then(data => res.json(data))
-    //     .then(data => console.log('fetched data:', data))
-    //     .catch(err => console.log(err))
-
-
-    // res.sendFile('test passed!')
-    res.redirect('/movies');
 }
 
 
-// const fetchMovies = (query) => {
-//     const apiKey = '6d16c97231f4eadcb5ba5ab27aa3338d'
-//     const query = editQuery(query)
-//     const url = editUrl(query, apiKey)
-//     fetch(url);
-// }
+const fetchMovies = (query) => {
+    const apiKey = '6d16c97231f4eadcb5ba5ab27aa3338d'
+    const editedQuery = editQuery(query)
+    const url = editUrl(editedQuery, apiKey)
+    return fetch(url);
+}
 
-// const editQuery = (query) => {
-//     return query.replaceAll(' ', '+')
-// }
+const editQuery = (query) => {
+    return query.replaceAll(' ', '+')
+}
 
-// const editUrl = (query, apiKey) => {
-//     return `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`
-// }
+const editUrl = (query, apiKey) => {
+    return `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`
+}
 
 
-module.exports = { getMovies, postMovies };
+module.exports = { getMovies, getApiMovies, postMovies };
